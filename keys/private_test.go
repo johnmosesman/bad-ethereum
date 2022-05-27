2ad54569e276fc999c3b8e4fb2f7380bced78a5d
John Mosesman <johnmosesman@gmail.com>
2022-05-26 21:22:54 -0500

    Add test

--- keys/private_test.go
+++ keys/private_test.go
@@ -1,1 +1,11 @@
 package keys
+
+import "testing"
+
+func TestGenPrivateKey(t *testing.T) {
+	key := GenPrivateKey()
+
+	if len(key) != 32 {
+		t.Errorf("Key is %d bytes, not 32 bytes", len(key))
+	}
+}
